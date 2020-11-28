6618884a91263cd08cf6d3ce2ba88604a6dcaa4e
ganganli <ganganli@naver.com>
2020-11-29 03:01:09 +0900

    상점 수정

--- ConsoleApplication1/StoreManager.cpp
+++ ConsoleApplication1/StoreManager.cpp
@@ -5,7 +5,7 @@
 
 //************************* Member Functions *************************
 
-enum SMChoice { SM_ZERO, SM_BUY, SM_RESET, SM_UPGRADE, SM_QUIT };
+enum SM_Choice { SM_ZERO, SM_BUY, SM_REFRESH, SM_UPGRADE, SM_INFO, SM_QUIT };
 
 void StoreManager::ShowStoreMenu()
 {
@@ -28,7 +28,8 @@ void StoreManager::ShowStoreMenu()
 		cout << "1. 아이템 구매" << endl;
 		cout << "2. 상점 새로고침" << endl;
 		cout << "3. 상점 업그레이드" << endl;
-		cout << "4. 돌아가기" << endl;
+		cout << "4. 상점 정보 확인" << endl;
+		cout << "5. 돌아가기" << endl;
 		cout << "-------------------------------------------------" << endl;
 		cout << "선택하세요. : " << endl;
 
@@ -50,7 +51,7 @@ void StoreManager::ShowStoreMenu()
 				system("cls");
 
 				ShowStoreList();
-
+				cout << "****************** 아이템 구매 ******************" << endl;
 				cout << "몇 번 슬롯의 아이템을 구매할까요?(1 ~ " << iMaxSlots << ", 돌아가기 : 0)" << endl; cin >> iChoice;
 
 				if (cin.fail()) {
@@ -75,14 +76,28 @@ void StoreManager::ShowStoreMenu()
 
 					char cConfirm;
 
-					cin >> cConfirm;
+					while (true) {
 
-					if (cin.fail()) {
-						cin.clear();
-						cin.ignore(1024, '\n');
-						continue;
+						cin >> cConfirm;
+
+						if (cin.fail()) {
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						if (cConfirm == 'y') {
+							cout << "『" << Slot[iChoice].GetItemName() << "』을(를) 구매했습니다. 남은 소지금 : ";
+							cPlayer.iGold -= Slot[iChoice].GetItemPrice();
+							cout << cPlayer.iGold << "G" << endl;
+							Slot[iChoice].Equip(cPlayer);
+							Slot[iChoice] = None;
+							system("PAUSE");
+							break;
+						}
+
+						if (cConfirm == 'n') break;
 					}
-
 				}
 				else {
 					cout << "잘못된 슬롯 선택입니다. 다시 선택해주세요." << endl;
@@ -93,18 +108,169 @@ void StoreManager::ShowStoreMenu()
 
 			break;
 
-		case SM_RESET:
-			cout << "상점의 목록을 새로고침합니다. ( 골드 : 00G )" << endl;
-			//아이템 리셋
+		case SM_REFRESH:
+			system("cls");
+
+			ShowStoreList();
+			cout << "****************** 상점 새로고침 ******************" << endl;
+			cout << "상점을 새로고침 할까요? 모든 슬롯이 새로고침 됩니다. 비용은 : " << iRefCost << "G 입니다.(y/n)" << endl;
+			
+			char cConfirm;
+
+			while (true) {
+
+				cin >> cConfirm;
+
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+
+				if (cConfirm == 'y') {
+					if (cPlayer.iGold < iRefCost) {
+						cout << "소지금이 부족합니다. 현재 소지금 : " << cPlayer.iGold << "G" << endl;
+						system("PAUSE");
+						break;
+					}
+					cPlayer.iGold -= iRefCost;
+					for (int i = 0; i < iMaxSlots; i++) {
+						FillSlot(i);
+					}
+					cout << "모든 슬롯이 새로고침 되었습니다." << endl;
+					system("PAUSE");
+					break;
+				}
+
+				if (cConfirm == 'n') break;
+			}
+
 			break;
 
 		case SM_UPGRADE:
-			cout << "상점 레벨을 업그레이드 합니다. ( 골드 : 00G )" << endl;
-			//상점 레벨업
-			break;
+			enum UPG_CHOICE {UP_NONE, UP_LVUP, UP_ADDSLOT, UP_QUIT};
 
+			while (true) {
+				system("cls");
+				cout << "****************** 상점 업그레이드 ******************" << endl;
+				cout << "1. 상점 레벨 업" << endl;
+				cout << "2. 상점 슬롯 늘리기" << endl;
+				cout << "3. 돌아가기" << endl;
+				cout << "******************************************" << endl;
+				cout << "선택하세요. : "; cin >> iChoice;
+
+				if (cin.fail()) {
+					cin.clear();
+					cin.ignore(1024, '\n');
+					continue;
+				}
+
+				if (iChoice == UP_QUIT) break;
+
+				switch(iChoice) {
+				case UP_LVUP: {
+					if (iStoreLevel == iStoreLevelLimit) {
+						cout << "더 이상 상점 레벨을 올릴 수 없습니다." << endl;
+						system("PAUSE");
+						break;
+					}
+
+					cout << "상점 레벨을 올릴까요? 비용은 : " << iStoreLevelUpCost << "G 입니다.(y/n)" << endl;
+
+					char cConfirm;
+
+					while (true) {
+
+						cin >> cConfirm;
+
+						if (cin.fail()) {
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						if (cConfirm == 'y') {
+							if (cPlayer.iGold < iStoreLevelUpCost) {
+								cout << "소지금이 부족합니다. 현재 소지금 : " << cPlayer.iGold << "G" << endl;
+								system("PAUSE");
+								break;
+							}
+							cPlayer.iGold -= iStoreLevelUpCost;
+							iStoreLevel++;
+							cout << "상점의 레벨이 올랐습니다. 현재 상점 레벨 : " << iStoreLevel << endl;
+							system("PAUSE");
+							break;
+						}
+
+						if (cConfirm == 'n') break;
+					}
+				}
+					
+					break;
+				case UP_ADDSLOT: {
+					if (iMaxSlots == iSlotLimit) {
+						cout << "더 이상 슬롯을 늘릴 수 없습니다." << endl;
+						system("PAUSE");
+						break;
+					}
+
+					cout << "상점 슬롯을 추가할까요? 비용은 : " << iAddSlotCost << "G 입니다.(y/n)" << endl;
+
+					char cConfirm;
+
+					while (true) {
+
+						cin >> cConfirm;
+
+						if (cin.fail()) {
+							cin.clear();
+							cin.ignore(1024, '\n');
+							continue;
+						}
+
+						if (cConfirm == 'y') {
+							if (cPlayer.iGold < iAddSlotCost) {
+								cout << "소지금이 부족합니다. 현재 소지금 : " << cPlayer.iGold << "G" << endl;
+								system("PAUSE");
+								break;
+							}
+							cPlayer.iGold -= iAddSlotCost;
+							iMaxSlots++;
+							cout << "상점 슬롯이 늘어났습니다. 현재 슬롯 수 : " << iMaxSlots << endl;
+							system("PAUSE");
+							break;
+						}
+
+						if (cConfirm == 'n') break;
+					}
+				}
+					break;
+				default:
+					cout << "잘못된 선택입니다. 다시 선택하세요." << endl;
+					system("PAUSE");
+					break;
+				}
+			}
+			break;
+		case SM_INFO:
+			system("cls");
+			cout << "****************** 상점 정보 ******************" << endl;
+			cout << "현재 상점 레벨 : " << iStoreLevel << endl;
+			cout << "현재 아이템 등장 확률 " << endl;
+			cout << "일반 : " << Odds[iStoreLevel][1] << "%" << endl;
+			cout << "마법 : " << Odds[iStoreLevel][2] << "%" << endl;
+			cout << "희귀 : " << Odds[iStoreLevel][3] << "%" << endl;
+			cout << "레어 : " << Odds[iStoreLevel][4] << "%" << endl;
+			cout << "유니크 : " << Odds[iStoreLevel][5] << "%" << endl << endl;
+			if (iStoreLevel < iStoreLevelLimit) cout << "다음 레벨 비용 : " << iStoreLevelUpCost << "G" << endl;
+			else cout << "현재 상점 레벨이 최대입니다." << endl << endl;
+			cout << "현재 상점 슬롯 수 : " << iMaxSlots << endl;
+			if (iMaxSlots < iSlotLimit) cout << "다음 슬롯 비용 : " << iAddSlotCost << "G" << endl;
+			else cout << "현재 상점 슬롯 수가 최대입니다." << endl << endl;
+			break;
 		default:
 			cout << "잘못된 입력입니다. 다시 선택하세요." << endl;
+			system("PAUSE");
 			break;
 
 		}
@@ -131,9 +297,10 @@ void StoreManager::FillSlot(int toFill) {
 	srand((unsigned int)time(NULL));
 
 	int iDice = (rand() % 100) + 1;
-	
+	int iChanceBar = 0;
 	for (int i = 1; i <= 6; i++) {
-		if (iDice <= Odds[iStoreLevel][i]) {
+		iChanceBar += Odds[iStoreLevel][i];
+		if (iDice <= iChanceBar) {
 			iDice = rand() % ItemList[i].size();
 			Slot[toFill] = ItemList[i][iDice];
 			return;
--- ConsoleApplication1/StoreManager.h
+++ ConsoleApplication1/StoreManager.h
@@ -60,23 +60,30 @@ class StoreManager
 {
 private:
 
-	int iStoreLevel = 1; //상점 레벨
-	int iMaxSlots = 3;
+	int		iStoreLevel = 1; //상점 레벨
+	int		iStoreLevelLimit = 9;
 
-	int Odds[10][6] = {
+	int		iMaxSlots = 3;
+	int		iSlotLimit = 10;
+
+	int		iRefCost = 50;
+	int		iStoreLevelUpCost = 500;
+	int		iAddSlotCost = 100;
+
+	int		Odds[10][6] = {
 		{0, 0, 0, 0, 0, 0},
-		{0, 90, 100, 0, 0, 0},			//1레벨
-		{0, 75, 95, 100, 0, 0},			//2레벨
-		{0, 60, 90, 100, 0, 0},			//3레벨
-		{0, 55, 85, 95, 100, 0},		//4레벨
-		{0, 45, 80, 95, 100, 0},		//5레벨
-		{0, 35, 70, 90, 100, 0},		//6레벨
-		{0, 25, 55, 85, 100, 0},		//7레벨
-		{0, 15, 40, 70, 95, 100},		//8레벨
-		{0, 10, 35, 60, 85, 100}		//9레벨
+		{0, 90, 10, 0, 0, 0},			//1레벨
+		{0, 75, 20, 5, 0, 0},			//2레벨
+		{0, 60, 30, 10, 0, 0},			//3레벨
+		{0, 55, 30, 10, 5, 0},		//4레벨
+		{0, 45, 35, 15, 5, 0},		//5레벨
+		{0, 35, 35, 20, 10, 0},		//6레벨
+		{0, 25, 30, 30, 15, 0},		//7레벨
+		{0, 15, 25, 30, 25, 5},		//8레벨
+		{0, 10, 25, 15, 35, 15}		//9레벨
 	};
 
-	vector<vector<Item>> ItemList{
+	vector<vector<Item>>	ItemList{
 		{None},
 		{LongSword, WoodenShield, ClothArmour, WoodenHelm},			//일반 등급
 		{Pickaxe, IronHelm, LeatherArmour, IronShield},				//마법 등급
@@ -85,7 +92,7 @@ private:
 		{DawnBlade, Tiara, MagePlate, DragonShield}					//유니크 등급
 	};
 
-	Item Slot[100];
+	Item	Slot[100];
 
 
 public:
